be14cef4cbf55d93aea0a7f4adcb46f08527454a
DAYANA <DAYANA@DESKTOP-OK7411E>
2021-05-02 00:02:08 -0500

    Clases agregadas

--- GestionAcademicaModel/src/pe/edu/pucp/gestion_academica/model/CitaOOIA.java
+++ GestionAcademicaModel/src/pe/edu/pucp/gestion_academica/model/CitaOOIA.java
@@ -1,14 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pe.edu.pucp.gestion_academica.model;
 
+package pe.edu.pucp.gestion_academica.model;
+import pe.edu.pucp.gestion_humana.model.Alumno;
+import pe.edu.pucp.gestion_humana.model.Horario;
+import pe.edu.pucp.gestion_humana.model.Persona;
 /**
  *
  * @author DAYANA
  */
 public class CitaOOIA {
+    private int id_cita;
+    private Alumno alumno;
+    private Horario horario;
+    private Motivo motivo;
+    private Persona asesor;
+
+    public CitaOOIA(int id_cita, Alumno alumno, Horario horario, Motivo motivo, Persona asesor) {
+        this.id_cita = id_cita;
+        this.alumno = alumno;
+        this.horario = horario;
+        this.motivo = motivo;
+        this.asesor = asesor;
+    }
+
+    public int getId_cita() {
+        return id_cita;
+    }
+
+    public void setId_cita(int id_cita) {
+        this.id_cita = id_cita;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Horario getHorario() {
+        return horario;
+    }
+
+    public void setHorario(Horario horario) {
+        this.horario = horario;
+    }
+
+    public Motivo getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(Motivo motivo) {
+        this.motivo = motivo;
+    }
+
+    public Persona getAsesor() {
+        return asesor;
+    }
+
+    public void setAsesor(Persona asesor) {
+        this.asesor = asesor;
+    }
+    
     
 }
--- GestionAcademicaModel/src/pe/edu/pucp/gestion_academica/model/Motivo.java
+++ GestionAcademicaModel/src/pe/edu/pucp/gestion_academica/model/Motivo.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pe.edu.pucp.gestion_academica.model;
 
 /**
@@ -10,5 +6,39 @@ package pe.edu.pucp.gestion_academica.model;
  * @author DAYANA
  */
 public class Motivo {
+    private int id_motivo;
+    private String codigoMotivo;
+    private String descripcion;
+
+    public Motivo(int id_motivo, String codigoMotivo, String descripcion) {
+        this.id_motivo = id_motivo;
+        this.codigoMotivo = codigoMotivo;
+        this.descripcion = descripcion;
+    }
+
+    public int getId_motivo() {
+        return id_motivo;
+    }
+
+    public void setId_motivo(int id_motivo) {
+        this.id_motivo = id_motivo;
+    }
+
+    public String getCodigoMotivo() {
+        return codigoMotivo;
+    }
+
+    public void setCodigoMotivo(String codigoMotivo) {
+        this.codigoMotivo = codigoMotivo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
     
 }
