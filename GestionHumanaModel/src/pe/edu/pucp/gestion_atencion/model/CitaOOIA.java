a8b0c4434e71534a886872fcef680d1a15b778f5
DAYANA <DAYANA@DESKTOP-OK7411E>
2021-05-04 16:05:13 -0500

    Clases agregadas

---
+++ GestionHumanaModel/src/pe/edu/pucp/gestion_atencion/model/CitaOOIA.java
@@ -0,0 +1,56 @@
+
+package pe.edu.pucp.gestion_atencion.model;
+import pe.edu.pucp.gestion_humana.model.Alumno;
+import pe.edu.pucp.gestion_academica.model.Horario;
+import pe.edu.pucp.gestion_humana.model.Persona;
+/**
+ *
+ * @author DAYANA
+ */
+public class CitaOOIA {
+    private int id_cita;
+    private Horario horario;
+    private Motivo motivo;
+    private Persona asesor;
+
+    public CitaOOIA(int id_cita, Horario horario, Motivo motivo, Persona asesor) {
+        this.id_cita = id_cita;
+        this.horario = horario;
+        this.motivo = motivo;
+        this.asesor = asesor;
+    }
+
+    public int getId_cita() {
+        return id_cita;
+    }
+
+    public void setId_cita(int id_cita) {
+        this.id_cita = id_cita;
+    }
+
+    public Horario getHorario() {
+        return horario;
+    }
+
+    public void setHorario(Horario horario) {
+        this.horario = horario;
+    }
+
+    public Motivo getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(Motivo motivo) {
+        this.motivo = motivo;
+    }
+
+    public Persona getAsesor() {
+        return asesor;
+    }
+
+    public void setAsesor(Persona asesor) {
+        this.asesor = asesor;
+    }
+    
+    
+}
