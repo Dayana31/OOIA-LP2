fdb59cd723798a0f1582d23610aef3d5042e1263
DAYANA <DAYANA@DESKTOP-OK7411E>
2021-05-11 21:14:42 -0500

    Clases corregidas

---
+++ GestionHumanaModel/src/pe/edu/pucp/gestion_atencion/model/Compromiso.java
@@ -0,0 +1,50 @@
+package pe.edu.pucp.gestion_atencion.model;
+
+/**
+ *
+ * @author DAYANA
+ */
+public class Compromiso {
+    private int id_compromiso;
+    private CitaOOIA cita;
+    private String descripcion;
+
+    public Compromiso(CitaOOIA cita, String descripcion) {
+        this.cita = cita;
+        this.descripcion = descripcion;
+    }
+    
+
+    public Compromiso(int id_compromiso, CitaOOIA cita, String descripcion) {
+        this.id_compromiso = id_compromiso;
+        this.cita = cita;
+        this.descripcion = descripcion;
+    }
+
+    public int getId_compromiso() {
+        return id_compromiso;
+    }
+
+    public void setId_compromiso(int id_compromiso) {
+        this.id_compromiso = id_compromiso;
+    }
+
+
+    public CitaOOIA getCita() {
+        return cita;
+    }
+
+    public void setCita(CitaOOIA cita) {
+        this.cita = cita;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+}
