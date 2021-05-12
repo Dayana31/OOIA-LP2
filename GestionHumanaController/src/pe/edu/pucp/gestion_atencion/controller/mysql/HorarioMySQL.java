9c5ff29f0fcf1451fa539e83b0ab3ccf449511c1
DAYANA <DAYANA@DESKTOP-OK7411E>
2021-05-12 10:56:23 -0500

    Clases corregidas

--- GestionHumanaController/src/pe/edu/pucp/gestion_atencion/controller/mysql/HorarioMySQL.java
+++ GestionHumanaController/src/pe/edu/pucp/gestion_atencion/controller/mysql/HorarioMySQL.java
@@ -1,11 +1,33 @@
 package pe.edu.pucp.gestion_atencion.controller.mysql;
 
+import java.util.ArrayList;
 import pe.edu.pucp.gestion_atencion.controller.dao.HorarioDAO;
+import pe.edu.pucp.gestion_atencion.model.Horario;
 
 /**
  *
  * @author DAYANA
  */
 public class HorarioMySQL implements HorarioDAO{
+
+    @Override
+    public ArrayList<Horario> listar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int insertar(Horario horario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int modificar(Horario horario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int eliminar(int idHorario) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
