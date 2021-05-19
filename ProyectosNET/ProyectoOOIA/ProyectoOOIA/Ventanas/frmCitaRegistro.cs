6521714113cbc5ddc515b3002aec79ee6676af0b
Dayana31 <70593166+Dayana31@users.noreply.github.com>
2021-05-19 13:06:51 -0500

    Citas

--- ProyectosNET/ProyectoOOIA/ProyectoOOIA/Ventanas/frmCitaRegistro.cs
+++ ProyectosNET/ProyectoOOIA/ProyectoOOIA/Ventanas/frmCitaRegistro.cs
@@ -17,5 +17,14 @@ namespace ProyectoOOIA.Ventanas
             InitializeComponent();
         }
 
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
---
+++ ProyectosNET/ProyectoOOIA/ProyectoOOIA/Ventanas/frmHorarioCita.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoOOIA.Ventanas
+{
+    public partial class frmHorarioCita : Form
+    {
+        public frmHorarioCita()
+        {
+            InitializeComponent();
+        }
+
+        private void bindingNavigatorMoveNextItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
