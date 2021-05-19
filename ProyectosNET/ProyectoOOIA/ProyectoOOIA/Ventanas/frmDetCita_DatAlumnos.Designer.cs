0da83374de3f3f380bf2416e3c51d989a407bc4e
Kotoamatsukam <70039162+Kotoamatsukam@users.noreply.github.com>
2021-05-19 18:03:01 -0500

    Agregue las pantallas de eventos y detalle cita
    Agregue las pantallas de Datos de alumno y ficha de atencion. Ademas agregue las pantallas principal de eventos, categorias y lista de eventos

--- ProyectosNET/ProyectoOOIA/ProyectoOOIA/Ventanas/frmDetCita_DatAlumnos.Designer.cs
+++ ProyectosNET/ProyectoOOIA/ProyectoOOIA/Ventanas/frmDetCita_DatAlumnos.Designer.cs
@@ -29,12 +29,320 @@ namespace ProyectoOOIA.Ventanas
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btDatosAlumno = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.lbEdad = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lbLugarDeNacimiento = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lbEspecialidad = new System.Windows.Forms.Label();
+            this.lbCorreos = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbNombreAlumno = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(356, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " Cita XXXX - 16.05.2021 8:00 am";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProyectoOOIA.Properties.Resources.home;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 35);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btDatosAlumno
+            // 
+            this.btDatosAlumno.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.btDatosAlumno.Location = new System.Drawing.Point(79, 45);
+            this.btDatosAlumno.Name = "btDatosAlumno";
+            this.btDatosAlumno.Size = new System.Drawing.Size(113, 51);
+            this.btDatosAlumno.TabIndex = 2;
+            this.btDatosAlumno.Text = "Datos de alumno";
+            this.btDatosAlumno.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.button1.Location = new System.Drawing.Point(214, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 51);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Ficha de atencion";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.button2.Location = new System.Drawing.Point(346, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 51);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Historia academica";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.button3.Location = new System.Drawing.Point(483, 45);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 51);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Eventos y citas";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.textBox7);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.textBox6);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.lbEdad);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.lbLugarDeNacimiento);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.lbEspecialidad);
+            this.panel1.Controls.Add(this.lbCorreos);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.lbNombreAlumno);
+            this.panel1.Location = new System.Drawing.Point(8, 113);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(780, 334);
+            this.panel1.TabIndex = 6;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ProyectoOOIA.Properties.Resources.Estudiante1;
+            this.pictureBox2.Location = new System.Drawing.Point(532, 19);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(234, 221);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 31;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(42, 355);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(103, 20);
+            this.textBox4.TabIndex = 30;
+            // 
+            // lbEdad
+            // 
+            this.lbEdad.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.lbEdad.Location = new System.Drawing.Point(25, 329);
+            this.lbEdad.Name = "lbEdad";
+            this.lbEdad.Size = new System.Drawing.Size(167, 23);
+            this.lbEdad.TabIndex = 29;
+            this.lbEdad.Text = "Edad";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(42, 292);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(208, 20);
+            this.textBox3.TabIndex = 28;
+            // 
+            // lbLugarDeNacimiento
+            // 
+            this.lbLugarDeNacimiento.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.lbLugarDeNacimiento.Location = new System.Drawing.Point(25, 266);
+            this.lbLugarDeNacimiento.Name = "lbLugarDeNacimiento";
+            this.lbLugarDeNacimiento.Size = new System.Drawing.Size(256, 23);
+            this.lbLugarDeNacimiento.TabIndex = 27;
+            this.lbLugarDeNacimiento.Text = "Lugar y fecha de nacimiento";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(42, 223);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(164, 20);
+            this.textBox2.TabIndex = 26;
+            // 
+            // lbEspecialidad
+            // 
+            this.lbEspecialidad.AccessibleRole = System.Windows.Forms.AccessibleRole.SplitButton;
+            this.lbEspecialidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.lbEspecialidad.Location = new System.Drawing.Point(25, 197);
+            this.lbEspecialidad.Name = "lbEspecialidad";
+            this.lbEspecialidad.Size = new System.Drawing.Size(167, 23);
+            this.lbEspecialidad.TabIndex = 25;
+            this.lbEspecialidad.Text = "Especialidad";
+            // 
+            // lbCorreos
+            // 
+            this.lbCorreos.FormattingEnabled = true;
+            this.lbCorreos.Location = new System.Drawing.Point(42, 129);
+            this.lbCorreos.Name = "lbCorreos";
+            this.lbCorreos.Size = new System.Drawing.Size(164, 43);
+            this.lbCorreos.TabIndex = 24;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label4.Location = new System.Drawing.Point(25, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 23);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Contacto";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(109, 20);
+            this.textBox1.TabIndex = 22;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label3.Location = new System.Drawing.Point(25, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 23);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Codigo del alumno";
+            // 
+            // lbNombreAlumno
+            // 
+            this.lbNombreAlumno.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.lbNombreAlumno.Location = new System.Drawing.Point(3, 0);
+            this.lbNombreAlumno.Name = "lbNombreAlumno";
+            this.lbNombreAlumno.Size = new System.Drawing.Size(294, 23);
+            this.lbNombreAlumno.TabIndex = 19;
+            this.lbNombreAlumno.Text = "Huertas Hortez, Jose Luis";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(42, 420);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(208, 20);
+            this.textBox5.TabIndex = 34;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label2.Location = new System.Drawing.Point(25, 394);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(256, 23);
+            this.label2.TabIndex = 33;
+            this.label2.Text = "Direccion";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(42, 479);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(41, 20);
+            this.textBox6.TabIndex = 36;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label6.Location = new System.Drawing.Point(25, 453);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(256, 23);
+            this.label6.TabIndex = 35;
+            this.label6.Text = "Cursos por segunda";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(42, 542);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(41, 20);
+            this.textBox7.TabIndex = 38;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label7.Location = new System.Drawing.Point(25, 516);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(256, 23);
+            this.label7.TabIndex = 37;
+            this.label7.Text = "Cursos por tercera";
+            // 
+            // frmDetCita_DatAlumnos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btDatosAlumno);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDetCita_DatAlumnos";
             this.Text = "frmDetCita_DatAlumnos";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btDatosAlumno;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label lbEdad;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lbLugarDeNacimiento;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lbEspecialidad;
+        private System.Windows.Forms.ListBox lbCorreos;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbNombreAlumno;
     }
 }
