cd4e57fc75f220ffb88245985b13e35c998d35cc
GerarrdN <a20140271@pucp.pe>
2021-05-19 13:43:55 -0500

    Formulario Historia Academica

--- ProyectosNET/ProyectoOOIA/ProyectoOOIA/Ventanas/frmDetCita_FichAlumnos.Designer.cs
+++ ProyectosNET/ProyectoOOIA/ProyectoOOIA/Ventanas/frmDetCita_FichAlumnos.Designer.cs
@@ -32,7 +32,7 @@ namespace ProyectoOOIA.Ventanas
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmDetCita_FichAlumnos";
+            this.Text = "Form1";
         }
 
         #endregion
