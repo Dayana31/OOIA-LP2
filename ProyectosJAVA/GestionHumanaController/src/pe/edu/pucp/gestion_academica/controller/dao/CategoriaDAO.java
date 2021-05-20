c198a65393ed3697b7c6358219b17a732b95dafa
Dayana31 <70593166+Dayana31@users.noreply.github.com>
2021-05-20 01:52:21 -0500

    CATEGORIA

--- ProyectosJAVA/GestionHumanaController/src/pe/edu/pucp/gestion_academica/controller/dao/CategoriaDAO.java
+++ ProyectosJAVA/GestionHumanaController/src/pe/edu/pucp/gestion_academica/controller/dao/CategoriaDAO.java
@@ -1,9 +1,15 @@
 package pe.edu.pucp.gestion_academica.controller.dao;
 
+import java.util.ArrayList;
+import pe.edu.pucp.gestion_academica.model.Categoria;
+
 /**
  *
  * @author Gerardo
  */
 public interface CategoriaDAO {
-    
+    public ArrayList<Categoria> listar();
+    public int insertar(Categoria categoria);
+    public int modificar(Categoria categoria);
+    public int eliminar(Categoria categoria);
 }
--- ProyectosJAVA/GestionHumanaController/src/pe/edu/pucp/gestion_academica/controller/mysql/CategoriaMySQL.java
+++ ProyectosJAVA/GestionHumanaController/src/pe/edu/pucp/gestion_academica/controller/mysql/CategoriaMySQL.java
@@ -1,9 +1,109 @@
 package pe.edu.pucp.gestion_academica.controller.mysql;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import pe.edu.pucp.config.DBManager;
+import pe.edu.pucp.gestion_academica.controller.dao.CategoriaDAO;
+import pe.edu.pucp.gestion_academica.model.Categoria;
+
 /**
  *
  * @author Gerardo
  */
-public class CategoriaMySQL {
+public class CategoriaMySQL implements CategoriaDAO {
+    
+    Connection con;
+    ResultSet rs;
+    CallableStatement cs;
+    
+    @Override
+    public ArrayList<Categoria> listar() {
+        ArrayList<Categoria> categorias = new ArrayList<>();
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(DBManager.url, DBManager.user, DBManager.password);
+            cs = con.prepareCall("{call listar_categoria()}");
+            rs = cs.executeQuery();
+            while(rs.next()){
+                Categoria categoria = new Categoria();
+                categoria.setId_categoria(rs.getInt("id_categoria"));
+                categoria.setNombre_categoria(rs.getString("nombre"));
+                
+                categorias.add(categoria);
+            }
+            rs.close();
+            cs.close();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{con.close();}catch(Exception ex){System.out.println(ex.getMessage());}
+        }
+        return categorias;
+    }
+
+    @Override
+    public int insertar(Categoria categoria) {
+        int resultado = 0;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(DBManager.url, DBManager.user, DBManager.password);
+            cs = con.prepareCall("{call insertar_categoria(?,?)}");
+            cs.registerOutParameter("_id_categoria", java.sql.Types.INTEGER);
+            /*Persona*/
+            cs.setString("_nombre", categoria.getNombre_categoria());
+            cs.executeUpdate();
+            categoria.setId_categoria(cs.getInt("_id_categoria"));
+            resultado = 1;
+            cs.close();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{con.close();}catch(Exception ex){System.out.println(ex.getMessage());}
+        }
+        return resultado;
+    }
+
+    @Override
+    public int modificar(Categoria categoria) {
+        int resultado = 0;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(DBManager.url, DBManager.user, DBManager.password);
+            cs = con.prepareCall("{call modificar_categoria(?,?)}");
+            cs.registerOutParameter("_id_categoria", java.sql.Types.INTEGER);
+            /*Persona*/
+            cs.setString("_nombre", categoria.getNombre_categoria());
+            cs.executeUpdate();
+            resultado = 1;
+            cs.close();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{con.close();}catch(Exception ex){System.out.println(ex.getMessage());}
+        }
+        return resultado;
+    }
+
+    @Override
+    public int eliminar(Categoria categoria) {
+        int resultado = 0;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(DBManager.url, DBManager.user, DBManager.password);
+            cs = con.prepareCall("{call eliminar_categoria(?)}");
+            cs.setInt("_id_categoria", categoria.getId_categoria());
+            cs.executeUpdate();
+            resultado = 1;
+            cs.close();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            try{con.close();}catch(Exception ex){System.out.println(ex.getMessage());}
+        }
+        return resultado;
+    }
     
 }
