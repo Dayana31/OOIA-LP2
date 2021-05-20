913442575525ef245f220aeb86a341532bfec151
Kotoamatsukam <70039162+Kotoamatsukam@users.noreply.github.com>
2021-05-20 00:30:40 -0500

    Se hizo la navegacion de eventos
    Se hicieron todas las navegaciones de eventos, por favor revisar.

--- ProyectosNET/ProyectoOOIA/ProyectoOOIA/Ventanas/frmPrincipalEventos.cs
+++ ProyectosNET/ProyectoOOIA/ProyectoOOIA/Ventanas/frmPrincipalEventos.cs
@@ -12,6 +12,7 @@ namespace ProyectoOOIA.Ventanas
 {
     public partial class frmPrincipalEventos : Form
     {
+
         public frmPrincipalEventos()
         {
             InitializeComponent();
@@ -31,6 +32,47 @@ namespace ProyectoOOIA.Ventanas
         private void frmPrincipalEventos_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new CategoriasEventos().Show();
+            this.Close();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Prueba", "cerrar");
+
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            new frmVistadeEvento(1).Show();
+            //this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Home_Click(object sender, EventArgs e)
+        {
+            new frmPrincipalAlumno().Show();
+            this.Close();
+           
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            new frmPrincipalAlumno().Show();
+            this.Close();
         }
 
         //private void InitializeComponent()
--- ProyectosNET/ProyectoOOIA/ProyectoOOIA/Ventanas/frmVistadeEvento.cs
+++ ProyectosNET/ProyectoOOIA/ProyectoOOIA/Ventanas/frmVistadeEvento.cs
@@ -12,9 +12,49 @@ namespace ProyectoOOIA.Ventanas
 {
     public partial class frmVistadeEvento : Form
     {
+        private int flag;
+        frmPrincipalEventos principalEventos = null;
+        frmListaDeEventos listaEventos = null;
         public frmVistadeEvento()
         {
             InitializeComponent();
+        }
+
+        public frmVistadeEvento(frmPrincipalEventos p)
+        {
+            principalEventos = p;
+            InitializeComponent();
+        }
+
+        public frmVistadeEvento(int flag)
+        {
+            this.flag = flag;
+            InitializeComponent();
+        }
+
+        public frmVistadeEvento(frmListaDeEventos p)
+        {
+            listaEventos = p;
+            InitializeComponent();
+        }
+        private void label7_Click(object sender, EventArgs e)
+        {
+            new frmInscripcionEvento(this.flag).Show();
+            //if (principalEventos != null)
+            //{
+            //    new frmInscripcionEvento(principalEventos).Show();
+            //}
+           // else
+           // {
+            //    new frmInscripcionEvento(listaEventos).Show();
+            //}
+            this.Close();
+        }
+
+        private void atras_Click(object sender, EventArgs e)
+        {
+            new frmPrincipalEventos().Show();
+            this.Close();
         }
     }
 }
