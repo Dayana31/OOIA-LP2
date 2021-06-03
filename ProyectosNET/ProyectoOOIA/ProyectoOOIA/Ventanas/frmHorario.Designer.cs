e9ef76c262b009b2ef4afc8c1630219dc6c16fb7
Dayana31 <70593166+Dayana31@users.noreply.github.com>
2021-06-03 12:27:34 -0500

    Opinión

--- ProyectosNET/ProyectoOOIA/ProyectoOOIA/Ventanas/frmHorario.Designer.cs
+++ ProyectosNET/ProyectoOOIA/ProyectoOOIA/Ventanas/frmHorario.Designer.cs
@@ -30,15 +30,9 @@ namespace ProyectoOOIA.Ventanas
         private void InitializeComponent()
         {
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHorario));
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnHome = new System.Windows.Forms.PictureBox();
             this.lblHorario = new System.Windows.Forms.Label();
-            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Estado = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Especialidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Hora = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Fecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dgvHorarioProf = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
@@ -49,6 +43,12 @@ namespace ProyectoOOIA.Ventanas
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.btnBuscarEstudiante = new System.Windows.Forms.Button();
+            this.Fecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Hora = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Especialidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Estado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Correo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnHome)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvHorarioProf)).BeginInit();
@@ -61,18 +61,18 @@ namespace ProyectoOOIA.Ventanas
             this.panel1.Controls.Add(this.lblHorario);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 73);
+            this.panel1.Size = new System.Drawing.Size(1067, 90);
             this.panel1.TabIndex = 2;
             // 
             // btnHome
             // 
             this.btnHome.Image = global::ProyectoOOIA.Properties.Resources.home;
-            this.btnHome.Location = new System.Drawing.Point(21, 39);
-            this.btnHome.Margin = new System.Windows.Forms.Padding(2);
+            this.btnHome.Location = new System.Drawing.Point(28, 48);
+            this.btnHome.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnHome.Name = "btnHome";
-            this.btnHome.Size = new System.Drawing.Size(26, 21);
+            this.btnHome.Size = new System.Drawing.Size(35, 26);
             this.btnHome.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.btnHome.TabIndex = 1;
             this.btnHome.TabStop = false;
@@ -83,40 +83,11 @@ namespace ProyectoOOIA.Ventanas
             this.lblHorario.AutoSize = true;
             this.lblHorario.Font = new System.Drawing.Font("Gill Sans MT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblHorario.ForeColor = System.Drawing.SystemColors.Window;
-            this.lblHorario.Location = new System.Drawing.Point(51, 35);
-            this.lblHorario.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblHorario.Location = new System.Drawing.Point(68, 43);
             this.lblHorario.Name = "lblHorario";
-            this.lblHorario.Size = new System.Drawing.Size(194, 30);
+            this.lblHorario.Size = new System.Drawing.Size(251, 38);
             this.lblHorario.TabIndex = 0;
             this.lblHorario.Text = "Horario de Atención";
-            //this.lblHorario.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // Nombre
-            // 
-            this.Nombre.HeaderText = "Nombre del Alumno";
-            this.Nombre.Name = "Nombre";
-            this.Nombre.Width = 250;
-            // 
-            // Estado
-            // 
-            this.Estado.HeaderText = "Estado";
-            this.Estado.Name = "Estado";
-            // 
-            // Especialidad
-            // 
-            this.Especialidad.HeaderText = "Especialidad";
-            this.Especialidad.Name = "Especialidad";
-            this.Especialidad.Width = 170;
-            // 
-            // Hora
-            // 
-            this.Hora.HeaderText = "Hora";
-            this.Hora.Name = "Hora";
-            // 
-            // Fecha
-            // 
-            this.Fecha.HeaderText = "Fecha";
-            this.Fecha.Name = "Fecha";
             // 
             // dgvHorarioProf
             // 
@@ -135,44 +106,48 @@ namespace ProyectoOOIA.Ventanas
             this.Hora,
             this.Especialidad,
             this.Estado,
-            this.Nombre});
+            this.Nombre,
+            this.Correo});
             this.dgvHorarioProf.EnableHeadersVisualStyles = false;
-            this.dgvHorarioProf.Location = new System.Drawing.Point(21, 129);
+            this.dgvHorarioProf.Location = new System.Drawing.Point(28, 159);
+            this.dgvHorarioProf.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dgvHorarioProf.Name = "dgvHorarioProf";
-            this.dgvHorarioProf.Size = new System.Drawing.Size(753, 309);
+            this.dgvHorarioProf.RowHeadersWidth = 51;
+            this.dgvHorarioProf.Size = new System.Drawing.Size(1004, 380);
             this.dgvHorarioProf.TabIndex = 0;
-            //this.dgvHorarioProf.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvHorarioProf_CellContentClick);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(699, 103);
+            this.button1.Location = new System.Drawing.Point(932, 127);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 1;
             this.button1.Text = "Modificar";
             this.button1.UseVisualStyleBackColor = true;
-            //this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(21, 103);
+            this.dateTimePicker1.Location = new System.Drawing.Point(28, 127);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.Size = new System.Drawing.Size(265, 22);
             this.dateTimePicker1.TabIndex = 3;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(354, 102);
+            this.textBox1.Location = new System.Drawing.Point(472, 126);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(227, 20);
+            this.textBox1.Size = new System.Drawing.Size(301, 22);
             this.textBox1.TabIndex = 4;
-            //this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(618, 102);
+            this.button2.Location = new System.Drawing.Point(824, 126);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 5;
             this.button2.Text = "Detalles";
             this.button2.UseVisualStyleBackColor = true;
@@ -181,58 +156,102 @@ namespace ProyectoOOIA.Ventanas
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Gill Sans MT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(17, 79);
+            this.label1.Location = new System.Drawing.Point(23, 97);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 21);
+            this.label1.Size = new System.Drawing.Size(110, 27);
             this.label1.TabIndex = 6;
             this.label1.Text = "Fecha y hora";
-            //this.label1.Click += new System.EventHandler(this.label1_Click_1);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Gill Sans MT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(350, 78);
+            this.label2.Location = new System.Drawing.Point(467, 96);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(62, 21);
+            this.label2.Size = new System.Drawing.Size(79, 27);
             this.label2.TabIndex = 7;
             this.label2.Text = "Nombre";
-            //this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(227, 102);
+            this.comboBox1.Location = new System.Drawing.Point(303, 126);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Size = new System.Drawing.Size(160, 24);
             this.comboBox1.TabIndex = 8;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Gill Sans MT", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(223, 78);
+            this.label3.Location = new System.Drawing.Point(297, 96);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(109, 21);
+            this.label3.Size = new System.Drawing.Size(138, 27);
             this.label3.TabIndex = 9;
             this.label3.Text = "Tipo de Horario";
             // 
             // btnBuscarEstudiante
             // 
-            this.btnBuscarEstudiante.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnBuscarEstudiante.BackgroundImage")));
             this.btnBuscarEstudiante.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnBuscarEstudiante.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnBuscarEstudiante.Location = new System.Drawing.Point(587, 102);
+            this.btnBuscarEstudiante.Location = new System.Drawing.Point(783, 126);
+            this.btnBuscarEstudiante.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnBuscarEstudiante.Name = "btnBuscarEstudiante";
-            this.btnBuscarEstudiante.Size = new System.Drawing.Size(25, 23);
+            this.btnBuscarEstudiante.Size = new System.Drawing.Size(33, 28);
             this.btnBuscarEstudiante.TabIndex = 10;
             this.btnBuscarEstudiante.UseVisualStyleBackColor = true;
             // 
+            // Fecha
+            // 
+            this.Fecha.HeaderText = "Fecha";
+            this.Fecha.MinimumWidth = 6;
+            this.Fecha.Name = "Fecha";
+            this.Fecha.Width = 125;
+            // 
+            // Hora
+            // 
+            this.Hora.HeaderText = "Hora";
+            this.Hora.MinimumWidth = 6;
+            this.Hora.Name = "Hora";
+            this.Hora.Width = 125;
+            // 
+            // Especialidad
+            // 
+            this.Especialidad.HeaderText = "Especialidad";
+            this.Especialidad.MinimumWidth = 6;
+            this.Especialidad.Name = "Especialidad";
+            this.Especialidad.Width = 170;
+            // 
+            // Estado
+            // 
+            this.Estado.HeaderText = "Estado";
+            this.Estado.MinimumWidth = 6;
+            this.Estado.Name = "Estado";
+            this.Estado.Width = 125;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre del Alumno";
+            this.Nombre.MinimumWidth = 6;
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Width = 250;
+            // 
+            // Correo
+            // 
+            this.Correo.HeaderText = "Correo ";
+            this.Correo.MinimumWidth = 6;
+            this.Correo.Name = "Correo";
+            this.Correo.Width = 150;
+            // 
             // frmHorario
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.btnBuscarEstudiante);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.comboBox1);
@@ -244,6 +263,7 @@ namespace ProyectoOOIA.Ventanas
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dgvHorarioProf);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmHorario";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Horario";
@@ -261,11 +281,6 @@ namespace ProyectoOOIA.Ventanas
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox btnHome;
         private System.Windows.Forms.Label lblHorario;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Estado;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Especialidad;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Hora;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Fecha;
         private System.Windows.Forms.DataGridView dgvHorarioProf;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
@@ -276,5 +291,11 @@ namespace ProyectoOOIA.Ventanas
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnBuscarEstudiante;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fecha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Hora;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Especialidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Estado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Correo;
     }
 }
