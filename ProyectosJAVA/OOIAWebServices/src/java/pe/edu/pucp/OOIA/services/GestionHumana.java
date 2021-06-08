92254c7790e20ae90ff67c5016e75380f3eeac73
alvcald61 <64240813+alvcald61@users.noreply.github.com>
2021-06-08 11:51:27 -0500

    servicios

---
+++ ProyectosJAVA/OOIAWebServices/src/java/pe/edu/pucp/OOIA/services/GestionHumana.java
@@ -0,0 +1,268 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.pucp.OOIA.services;
+
+import java.util.ArrayList;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import pe.edu.pucp.gestion_humana.controller.dao.AlumnoDAO;
+import pe.edu.pucp.gestion_humana.controller.dao.EspecialidadDAO;
+import pe.edu.pucp.gestion_humana.controller.dao.InvitadoDAO;
+import pe.edu.pucp.gestion_humana.controller.dao.ProfesorDAO;
+import pe.edu.pucp.gestion_humana.controller.dao.PsicologoDAO;
+import pe.edu.pucp.gestion_humana.controller.mysql.AlumnoMySQL;
+import pe.edu.pucp.gestion_humana.controller.mysql.EspecialidadMySQL;
+import pe.edu.pucp.gestion_humana.controller.mysql.InvitadoMySQL;
+import pe.edu.pucp.gestion_humana.controller.mysql.ProfesorMySQL;
+import pe.edu.pucp.gestion_humana.controller.mysql.PsicologoMySQL;
+import pe.edu.pucp.gestion_humana.model.Alumno;
+import pe.edu.pucp.gestion_humana.model.Especialidad;
+import pe.edu.pucp.gestion_humana.model.Invitado;
+import pe.edu.pucp.gestion_humana.model.Profesor;
+import pe.edu.pucp.gestion_humana.model.Psicologo;
+
+/**
+ *
+ * @author LENOVO
+ */
+@WebService(serviceName = "gestionHumana")
+public class GestionHumana {
+
+    private ProfesorDAO daoProfesor;
+    private AlumnoDAO alumno;
+    private EspecialidadDAO especialidad;    
+    private PsicologoDAO psicologo;
+    private InvitadoDAO invitado;
+        public GestionHumana(){
+            daoProfesor=new ProfesorMySQL();
+            alumno=new AlumnoMySQL();
+            especialidad=new EspecialidadMySQL();
+            invitado=new InvitadoMySQL();
+            psicologo=new PsicologoMySQL();
+        }
+    @WebMethod(operationName = "listarProfesores")
+    public ArrayList <Profesor>listarProfesores(){
+        ArrayList empleador=null;
+        try {
+            empleador= daoProfesor.listar();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return empleador;
+    }
+    @WebMethod(operationName = "insertarProfesores")
+    public int insertarProfesores( @WebParam(name="profesor") Profesor profesor){
+        int resultado=0;
+        try {
+            resultado=daoProfesor.insertar(profesor);
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return resultado;
+    }
+    @WebMethod(operationName = "modificarProfesores")
+    public int modificarProfesores(@WebParam(name = "profesor")Profesor profesor){
+        int resultado=0;
+        try {
+            resultado=daoProfesor.modificar(profesor);
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return resultado;
+    }
+    @WebMethod(operationName = "eliminarProfesor")
+    public int eliminarProfesor(@WebParam(name = "profesor")Profesor profesor){
+        int resultado=0;
+        try {
+            resultado=daoProfesor.eliminar(profesor);
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        return resultado;
+    }
+    
+
+    @WebMethod(operationName = "listarAlumno")
+    public ArrayList<Alumno> listarAlumno(){
+        ArrayList lista=null;
+        try {
+            lista=alumno.listar();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return lista;
+        
+    }
+    @WebMethod(operationName = "insertarAlumno")
+    public int insertarAlumno(@WebParam(name = "alumno")Alumno alumno){
+        int resultado=0;
+        try {
+            resultado=this.alumno.insertar(alumno);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return resultado;
+    }
+    @WebMethod(operationName = "modificarAlumno")
+    public int modificarAlumno(@WebParam(name = "alumno")Alumno alumno){
+        int resultado=0;
+        try {
+            resultado=this.alumno.modificar(alumno);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return resultado;
+    }
+    @WebMethod(operationName = "eliminarAlumno")
+    public int eliminarAlumno(@WebParam(name = "alumno")Alumno alumno){
+        int resultado=0;
+        try {
+            resultado=this.alumno.eliminar(alumno);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return resultado;
+    }
+    
+    @WebMethod(operationName = "listarEsppecialidad")
+    public ArrayList<Especialidad> listarEspecialidad(){
+        ArrayList<Especialidad>listaEspecialidad=null;
+        try {
+            listaEspecialidad= this.especialidad.listar();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return listaEspecialidad;
+    }
+    @WebMethod(operationName = "insertarEspecialidad")
+    public int insertarEspecialidad(@WebParam(name = "especialidad")Especialidad especialidad){
+        int resultado=0;
+        try {
+            resultado=this.especialidad.insertar(especialidad);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return resultado;
+    }
+    @WebMethod(operationName = "modificarEspecialidad")
+    public int modificarEspecialidad(@WebParam(name = "especialidad")Especialidad especialidad){
+        int resultado=0;
+        try {
+            resultado=this.especialidad.modificar(especialidad);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return resultado;
+    }
+    @WebMethod(operationName = "eliminarEspecialidad")
+    public int eliminarEspecialidad(@WebParam(name = "especialidad")Especialidad especialidad){
+        int resultado=0;
+        try {
+            resultado=this.especialidad.eliminar(especialidad);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return resultado;
+    }
+    
+    @WebMethod(operationName = "listarInvitado")
+    public ArrayList<Invitado> listarInvitado(){
+        ArrayList<Invitado> lista=null;
+        try {
+            lista=invitado.listar();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return lista;
+    }
+    @WebMethod(operationName = "insertarInvitado")
+    public int insertarInvitado(@WebParam(name = "invitado")Invitado invitado){
+        int resultado=0;
+        try {
+            resultado=this.invitado.insertar(invitado);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return resultado;
+        
+    }
+    @WebMethod(operationName = "modificarInvitado")
+    public int modificarInvitado(@WebParam(name = "invitado")Invitado invitado){
+        int resultado=0;
+        try {
+            resultado=this.invitado.modificar(invitado);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return resultado;
+        
+    }
+    @WebMethod(operationName = "eliminarInvitado")
+    public int eliminarInvitado(@WebParam(name = "invitado")Invitado invitado){
+        int resultado=0;
+        try {
+            resultado=this.invitado.eliminar(invitado);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return resultado;
+        
+    }
+
+
+    @WebMethod(operationName = "listarPsicologo")
+    public ArrayList<Psicologo> listarPsicologo(){
+        ArrayList<Psicologo> lista=null;
+        try {
+            lista=psicologo.listar();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return lista;
+    }
+    @WebMethod(operationName = "insertarPsicologo")
+    public int insertarPsicologo(@WebParam(name = "invitado")Psicologo psicologo){
+        int resultado=0;
+        try {
+            resultado=this.psicologo.insertar(psicologo);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return resultado;
+        
+    }
+    @WebMethod(operationName = "modificarPsicologo")
+    public int modificarPsicologo(@WebParam(name = "invitado")Psicologo psicologo){
+        int resultado=0;
+        try {
+            resultado=this.psicologo.modificar(psicologo);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return resultado;
+        
+    }
+    @WebMethod(operationName = "eliminarPsicologo")
+    public int eliminarPsicologo(@WebParam(name = "invitado")Psicologo psicologo){
+        int resultado=0;
+        try {
+            resultado=this.psicologo.eliminar(psicologo);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return resultado;
+        
+    }
+    
+    
+}
